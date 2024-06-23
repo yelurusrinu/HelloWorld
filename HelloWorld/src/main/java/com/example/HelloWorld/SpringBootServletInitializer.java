7f17b21b5a73979d3038d224bc025b787197374f
Yeluru Sreenivasulu <96856227+yelurusrinu@users.noreply.github.com>
2024-06-23 11:10:56 +0530

    Update SpringBootServletInitializer.java

--- HelloWorld/src/main/java/com/example/HelloWorld/SpringBootServletInitializer.java
+++ HelloWorld/src/main/java/com/example/HelloWorld/SpringBootServletInitializer.java
@@ -1,19 +1,1 @@
-package com.example.helloworld;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-@SpringBootApplication
-public class HelloWorldApplication extends SpringBootServletInitializer {
-
-    public static void main(String[] args) {
-        SpringApplication.run(HelloWorldApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(HelloWorldApplication.class);
-    }
-}
